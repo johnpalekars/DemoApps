3e8b436736aad897bf9ae86ecd96d15e3f637026
johnpalekars <johnpalekars@gmail.com>
2022-10-03 13:43:31 +0530

    minor changes

--- src/components/chatApp/ChatApp.js
+++ src/components/chatApp/ChatApp.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./ChatApp.css";
 const ChatApp = () => {
   return (
-    <div
-      className="container flex"
-      style={{ margin: "150px 100px 2px 100px", padding: "20px", color:"white"}}
-    >
+    <Fragment>
       <div className="form-container">
-        <div className="form-group text-center " >
-          <h1>{`<>DevRooms</>`}</h1>
-          <input className="form-control input" type="text" placeholder="Username..." />
-          <select className="form-control input">
-            <option>-- Select Room --</option>
-            <option value="javascript">JavaScript</option>
-            <option value="AmongUs">AmongUs</option>
-            <option value="node">Node</option>
-            <option value="express">Express</option>
-            <option value="react">React</option>
-          </select>
-          <button className="btn btn-secondary input">Join Room</button>
+        <div className="container flex" style={{ color: "white" }}>
+          <div className="form-group text-center ">
+            <h1>{`<>DevRooms</>`}</h1>
+            <input
+              className="form-control input"
+              type="text"
+              placeholder="Username..."
+            />
+            <select className="form-control input">
+              <option>-- Select Room --</option>
+              <option value="javascript">JavaScript</option>
+              <option value="AmongUs">AmongUs</option>
+              <option value="node">Node</option>
+              <option value="express">Express</option>
+              <option value="react">React</option>
+            </select>
+            <button className="btn btn-secondary input">Join Room</button>
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,21 +1,55 @@
 import React, { Fragment } from "react";
-import { useSelector,useDispatch } from "react-redux";
-import {counterINC} from "../../Store/reducers/counter"; 
+import { useSelector, useDispatch } from "react-redux";
+import { counterINC } from "../../Store/reducers/counter";
 import { increment, decrement } from "../../Store/reducers/counter";
 
 const Counter = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-    const counter = useSelector(state => state.counter.value);
+  const counter = useSelector((state) => state.counter.value);
 
   return (
     <Fragment>
-      <div style={{color:"white",}}>{counter}</div>
-        <br />
-      <button onClick={()=>dispatch(decrement())}>-</button>
-      <button onClick={()=>dispatch(increment())}>+</button>
-      <button onClick={()=>dispatch(counterINC.actions.incrementByTen())}>+10</button>
+      <div class="row h1">
+        {" "}
+        <div style={{ color: "white", fontSize: "100px" }} class="text-center">
+          {counter}
+        </div>
+      </div>
+      <br />
+      {[1, 1, 1].map(() => (
+        <hr style={{ color: "white" }} />
+      ))}
+      <br />
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-4 text-center">
+            <button
+              className="btn btn-lg btn-danger"
+              onClick={() => dispatch(decrement())}
+            >
+              -
+            </button>
+          </div>
+          <div className="col-4 text-center">
+            <button
+              className="btn btn-lg btn-primary"
+              onClick={() => dispatch(increment())}
+            >
+              +
+            </button>
+          </div>
+          <div className="col-4 text-center">
+            <button
+              className="btn btn-lg btn-primary"
+              onClick={() => dispatch(counterINC.actions.incrementByTen())}
+            >
+              +10
+            </button>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
